1ef7b93c1283d35baca96a42c55782e318f30709
Pablo León <pavel.leon@gmail.com>
2016-04-25 10:07:05 -0300

    - cambiar algoritmo de reordenamiento, cambia loop principal.

--- NumerosPalabras/BuchstabierenZahlen.pl
+++ NumerosPalabras/BuchstabierenZahlen.pl
@@ -4,14 +4,14 @@
 
 =head1 NOMBRE
 
-xnum1.pl - 
+BuchstabierenZahlen.pl - 
 
 =head1 SINOPSIS
 
-   xnum1.pl [switches]
+   BuchstabierenZahlen.pl [switches]
       --help         Obtener ayuda
       --debug        Mostrar mensajes de debug (STDERR)
-      --trace        Guardar mensajes de debug en xnum1.pl.trc
+      --trace        Guardar mensajes de debug en BuchstabierenZahlen.pl.trc
       --sqltech=dir  Directorio de herramientas (SQLTECHHOME)
 
 =head1 DESCRIPCION
@@ -43,6 +43,8 @@ use Data::Dumper;
 use Getopt::Long ();
 
 use utf8;
+
+require 'lib_buchstab.pl';
 
 
 sub help () {
@@ -140,13 +142,8 @@ my ($rc);
 #
 # Init
 #
-sub Num2De($);
-sub Test_Num2De();
-sub Test_GenNumSeq();
-sub GenNumSeq($$);
 
-my $sep='';
-$sep=' ' if($optctl->{'sep'});
+SetSep(' ') if($optctl->{'sep'});
 
 my $nDigits=$optctl->{'digits'};
 
@@ -211,63 +208,31 @@ my $nLimit=10**$nDigits;
 if ($optctl->{'test'}) {
 	Test_Num2De();
 	Test_GenNumSeq();
+	Test_Shuffle();
 } else {
 
 
 for (my $nSet=1; $nSet <= $nSets; $nSet+=1) 
 {
-	my @aNumbers=();
-	my $rToWrite={};
-
 	print QF "\n\n### Set $nSet\n\n";
 	print AF "\n\n### Set $nSet\n\n";
 
 	my @aSeq= GenNumSeq($nDigits, $nSetSize);
 	&{$::debug}("aSeq:[%s]", Dumper(\@aSeq));
+	my @aShuf=Shuffle(@aSeq);
+	&{$::debug}("aShuf:[%s]", Dumper(\@aShuf));
 
-	for(my $i=0; $i < scalar(@aSeq); $i+=1) {
-		my $n=0+$aSeq[$i];
-		my @aPair=($n, Num2De($n));
-		push @aNumbers, \@aPair;
-		$rToWrite->{$i}=$n;
+	for (my $i=0; $i < scalar(@aSeq); $i+=1) {
+		# print $aSeq[$i], " --- ", $aShuf[$i], "\n";
+		my ($this, $that)=(0+$aSeq[$i], 0+$aShuf[$i]);
 
 		print AF sprintf("- %60s *-----> %*d\n\n"
-			,$aPair[1], , $nDigits, $aPair[0]);
-		# print AF sprintf("- %9d *----------> %s\n\n",@aPair);
-	}
-	&{$::debug}("aNumbers:[%s]", Dumper(\@aNumbers));
-	&{$::debug}("rToWrite:[%s]", Dumper($rToWrite));
-
-	my $j;
-	for(my $i=0; $i < $nSetSize; $i+=1)
-	{
-		my $this=$aNumbers[$i]->[0];
-		&{$::debug}("i:[$i] this:[%s]", $this);
-		
-		my @aOthers= keys(%{$rToWrite});
-		&{$::debug}("aOthers:[%s]", Dumper(\@aOthers));
-		if (scalar(@aOthers) == 1) {
-			$j = $aOthers[0];
-		} else {
-			do { 
-				$j = $aOthers[int(rand(scalar(@aOthers)))];
-				&{$::debug}("j:[$j]");
-			} while($j == $i);
-		}
-		delete($rToWrite->{$j});
-
-		my $that=$aNumbers[$j]->[0];
-		my $words=$aNumbers[$j]->[1];
-
-		&{$::debug}("j:[$j] that:[%s] words:[%s]"
-			,$words, $that);
+			,Num2De($this), $nDigits, $this );
 
 		print QF sprintf("- %60s *     > %*d\n\n"
-			,$words, $nDigits, $this);
-		# print QF sprintf("- %9d *          > %s\n\n"
-		# 	,$this, $words);
-
+			,Num2De($this), $nDigits, $that );
 	}
+
 }
 }
 	
@@ -294,216 +259,5 @@ sub Funcion
 	return $ret;
 }
 
-
-sub Test_GenNumSeq()
-{
-	my ($dummy)=@_;
-	my $ret='';
-	
-	&{$::debug}("Test_GenNumSeq( %s )", join(' ', map( (defined($_)?"[$_]":'<undef>'), @_)));
-
-	# Cuerpo de Funcion
-	my @a;
-	@a = GenNumSeq(1,1);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-
-	@a = GenNumSeq(1,4);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-
-	@a = GenNumSeq(2,4);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-
-	@a = GenNumSeq(3,4);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-
-	@a = GenNumSeq(3,6);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-	
-	@a = GenNumSeq(4,6);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-	
-	&{$::debug}("Test_GenNumSeq()->[%s]", $ret);
-	return $ret;
-}
-
-sub GenNumSeq($$)
-{
-	my ($nNumLen, $nSeqLen)=@_;
-	my $ret='';
-	
-	&{$::debug}("GenNumSeq( %s )", join(' ', map( (defined($_)?"[$_]":'<undef>'), @_)));
-
-	# Cuerpo de Funcion
-	my $nLimit=10**$nNumLen;
-	my $sNumber = sprintf("%0*d", $nNumLen, int(rand($nLimit)));
-	&{$::debug}("sNumber:[%s]", $sNumber);
-	
-	my $rhNum={ $sNumber => 1};
-
-	my ($i, $sOldDigit, $sNewDigit, $nPos);
-	while (scalar(keys(%{$rhNum})) < $nSeqLen) {
-		$nPos = int(rand($nNumLen));
-		$sOldDigit=substr($sNumber, $nPos, 1);
-		do {
-			$sNewDigit = '' . int(rand(10));
-		} while ($sNewDigit eq $sOldDigit);
-		substr($sNumber, $nPos, 1)=$sNewDigit;
-		&{$::debug}("sNumber:[%s]", $sNumber);
-		$rhNum->{$sNumber}=1;
-	}
-	
-	my @aSeq=keys(%{$rhNum});
-	
-	&{$::debug}("GenNumSeq()->[%s]", \@aSeq);
-	return @aSeq;
-}
-
-
-sub Test_Num2De()
-{
-	my ($dummy)=@_;
-	my $ret='';
-	
-	&{$::debug}("Test_Num2De( %s )", join(' ', map( (defined($_)?"[$_]":'<undef>'), @_)));
-
-	# Cuerpo de Funcion
-	my @aNums;
-	#@aNums= (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-	#	,21,29,33,40,49,50,51,66,77,83,99);
-	#@aNums= (100,101,202,330,404,505,676,787,898,909);
-	#@aNums= (101,21);
-	# @aNums= (1000,2001,2023,3034,4045,5056,6767,7878,8989,9090);
-	@aNums= (0,1,2,10,11,19,20
-		,21,99,100,101,345,421,603,840,999
-		,1000,1001,2007,3010,9999,10000,10001,10010,10100,11000
-		,99999, 100000, 100001
-		,123456, 1234567, 2345678, 999999999
-		) ;
-
-	foreach my $n (@aNums) {
-		&{$::debug}("n:[%s] words:[%s]", $n, Num2De($n));
-		print sprintf("n:[%s] %s\n", $n, Num2De($n));
-	}
-	
-	&{$::debug}("Test_Num2De()->[%s]", $ret);
-	return $ret;
-}
-
-
-sub Num2De($)
-{
-	my ($num)=@_;
-	my $ret='';
-	
-	&{$::debug}("Num2De( %s )", join(' ', map( (defined($_)?"[$_]":'<undef>'), @_)));
-
-	# Cuerpo de Funcion
-	my (%cipher, %hTens);
-	%cipher = (	1 => "ein", 2 => "zwei", 3 => "drei"
-		,4 => "vier", 5 => "fünf", 6 => "sechs"
-		,7 => "sieben",8 => "acht", 9 => "neun"
-		,10 => "zehn", 11 => "elf", 12 => "zwölf"
-		,13 => "dreizehn", 14 => "veirzehn", 15 => "fünfzehn"
-		,16 => "sechzehn", 17 => "siebzehn", 18 => "achzehn"
-		,19 => "neunzehn" );
-	%hTens = (	1 => "zehn", 2 => "zwanzig",3 => "dreissig"
-		,4 => "vierzig", 5 => "fünfzig", 6 => "sechzig"
-		,7 => "siebzig", 8 => "achzig", 9 => "neunzig" );
-
-	my ($tens, $hundreds)=('','');
-
-	# (my $tens = $num) =~ s/^.*(\d\d)$/$1/;
-
-	if( $num == 0 ) {
-		$ret = "null";
-	} elsif( $num == 1 ) {
-		$ret = "eins";
-	} elsif( $num < 20 ) {
-		$ret = $cipher{$num} || "null";
-	} elsif( $num < 100 ) {
-		my $nRes=$num%10;
-		my $nTens=($num-$nRes)/10;
-		$ret = ((($nRes == 1)? 'ein' : $cipher{$nRes})
-				.${sep}."und".${sep} )
-			if ($nRes>0);
-		$ret.= $hTens{$nTens};
-	} elsif( $num < 1000 ) {
-		my $nRes=$num%100;
-		my $nHund=($num-$nRes)/100;
-		$ret = $cipher{$nHund}.${sep}."hundert";
-		$ret.= ${sep}.Num2De($nRes)
-			if ($nRes>0);
-	} elsif( $num < 1000000 ) {
-		my $nRes=$num%1000;
-		my $nTaus=($num-$nRes)/1000;
-		$ret = Num2De($nTaus).${sep} ."tausend";
-		$ret.= ${sep}.Num2De($nRes)
-			if ($nRes>0);
-	} elsif( $num < 1000000000 ) {
-		my $nRes=$num%1000000;
-		my $nMill=($num-$nRes)/1000000;
-		$ret='ein Million';
-		$ret=Num2De($nMill).${sep}.'Millionen'
-			if ($nMill>1);
-		$ret.= ${sep}.Num2De($nRes)
-			if ($nRes>0);
-	} else {
-		$ret=$num;
-
-	}
-	
-	&{$::debug}("Num2De()->[%s]", $ret);
-	return $ret;
-}
-
-# /usr/local/share/perl5/Date/Spoken/German.pm
-# sub yeartospoken
-sub yeartospoken
-{
-	my $year = shift;
-	(my $tens = $year) =~ s/^.*(\d\d)$/$1/;
-
-	my %cipher = (	1 => "ein", 2 => "zwei", 3 => "drei"
-		,4 => "vier", 5 => "fünf", 6 => "sechs"
-		,7 => "sieben",8 => "acht", 9 => "neun"
-		,10 => "zehn", 11 => "elf", 12 => "zwölf"
-		,13 => "dreizehn", 14 => "veirzehn", 15 => "fünfzehn"
-		,16 => "sechzehn", 17 => "siebzehn", 18 => "achzehn"
-		,19 => "neunzehn" );
-	my %hTens = (	1 => "zehn", 2 => "zwanzig",3 => "dreissig"
-		,4 => "vierzig", 5 => "fünfzig", 6 => "sechzig"
-		,7 => "siebzig", 8 => "achzig", 9 => "neunzig" );
-
-
-	my $hundreds = "";
-	if( $year < 10 ) {
-		$year = $cipher{$year} || "null";
-	} else {
-		if( $tens == 0 ) {
-			$tens = "";
-		} elsif( ($tens % 10) == 0 ) {
-			$tens =~ s/(.)(.)/$hTens{$1}/;
-		} else {
-			if( $tens < 10 ) {
-				$tens =~ s/(.)(.)/$cipher{$2}/;
-			} else {
-				$tens =~ s/(.)(.)/$cipher{"$1$2"} || $cipher{$2}.(($1==1)?$hTens{$1}:"und".$hTens{$1})/e;
-			}
-		}
-		if( $year >= 100 ) {
-			($hundreds = $year) =~ s/^(.?.)..$/$1/;
-			if( $hundreds >= 20 || $hundreds == 10 ) {
-				$hundreds =~ s/(.)(.)/$cipher{$1}."tausend".(($2>0)?$cipher{$2}."hundert":"")/ex;
-			} else {
-				if( $hundreds > 10 ) {
-					$hundreds =~ s/(.)(.)/($cipher{"$1$2"} || $cipher{$2}."zehn")."hundert"/e;
-				} else {
-					$hundreds = $cipher{$hundreds}."hundert";
-				}
-			}
-		}
-	}
-	return $hundreds.$tens;
-}
 
 
--- NumerosPalabras/xnum1.pl
+++ NumerosPalabras/xnum1.pl
@@ -44,6 +44,9 @@ use Getopt::Long ();
 
 use utf8;
 
+require 'lib_buchstab.pl';
+
+
 
 sub help () {
 	my ($pack, $file, $line)=caller();
@@ -140,138 +143,26 @@ my ($rc);
 #
 # Init
 #
-sub Num2De($);
-sub Test_Num2De();
-sub Test_GenNumSeq();
-sub GenNumSeq($$);
 
-my $sep='';
-$sep=' ' if($optctl->{'sep'});
-
-my $nDigits=$optctl->{'digits'};
-
-die "Parametro --digitos:[$nDigits] supre máximo de 9."
-	if($nDigits >9);
 
 #
 # Body
 #
 
+# Test_Shuffle();
 
+sub Sep();
+sub SepSep($);
 
-#
-# Init
-#
+SepSep('~');
 
-# use Date::Spoken::German;
-# use Lingua::EN::Numbers qw(num2en num2en_ordinal);
-
-# my $x = 234;
-# my $y = 54;
-# print "You have ", num2en($x), " things to do today!\n";
-# print "You will stop caring after the ", num2en_ordinal($y), ".\n";
+print "[", Sep(), "]", "\n"; 
 
 #
+# Close
 #
-#
-
-print "\n\n";
- 
-# print timetospoken( time() ), "\n";
-# print datetospoken( 1, 1, 9086 ), "\n";
-
-my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) =
-                                                localtime(time);
-$mon+=1;
-$year+=1900;
-
-my $sTimestamp=sprintf("%02d%02d%02d-%02d%02d"
-	,$year%100, $mon, $mday, $hour, $min  );
-$sTimestamp=sprintf("%02d%02d%02d-%02d"
-	,$year%100, $mon, $mday, $hour  );
-&{$::debug}("sTimestamp:[$sTimestamp]");
-
-my $sQFile=sprintf("Nummern-%s-q.txt", $sTimestamp);
-open QF, ">:utf8", $sQFile
-	or die "No se pudo abrir archivo [$sQFile] para escritura. $!";
-print QF "### Nummern -- Preguntas ($sTimestamp)\n\n";
-
-my $sAFile=sprintf("Nummern-%s-a.txt", $sTimestamp);
-open AF, ">:utf8", $sAFile
-	or die "No se pudo abrir archivo [$sAFile] para escritura. $!";
-print AF "### Nummern -- Respuestas ($sTimestamp)\n\n";
 
 
-my $nSets=11;
-
-my $nSetSize=4;
-my $nLimit=10**$nDigits;
-&{$::debug}("nDigits:[$nDigits] nLimit:[$nLimit]");
-
-if ($optctl->{'test'}) {
-	Test_GenNumSeq();
-} else {
-
-
-for (my $nSet=1; $nSet < $nSets; $nSet+=1) 
-{
-	my @aNumbers=();
-	my $rToWrite={};
-
-	print QF "\n\n### Set $nSet\n\n";
-	print AF "\n\n### Set $nSet\n\n";
-
-	for( my $i=0; $i < $nSetSize; $i+=1)
-	{
-		my $n;
-		do { 
-			$n = int(rand($nLimit));
-		} while ($n < 10);
-
-		my @aPair=($n, Num2De($n));
-		push @aNumbers, \@aPair;
-		$rToWrite->{$i}=$n;
-
-		print AF sprintf("- %9d *----------> %s\n\n",@aPair);
-
-	}
-	&{$::debug}("aNumbers:[%s]", Dumper(\@aNumbers));
-	&{$::debug}("rToWrite:[%s]", Dumper($rToWrite));
-
-	my $j;
-	for(my $i=0; $i < $nSetSize; $i+=1)
-	{
-		my $this=$aNumbers[$i]->[0];
-		&{$::debug}("i:[$i] this:[%s]", $this);
-		
-		my @aOthers= keys(%{$rToWrite});
-		&{$::debug}("aOthers:[%s]", Dumper(\@aOthers));
-		if (scalar(@aOthers) == 1) {
-			$j = $aOthers[0];
-		} else {
-			do { 
-				$j = $aOthers[int(rand(scalar(@aOthers)))];
-				&{$::debug}("j:[$j]");
-			} while($j == $i);
-		}
-		delete($rToWrite->{$j});
-
-		my $that=$aNumbers[$j]->[0];
-		my $words=$aNumbers[$j]->[1];
-
-		&{$::debug}("j:[$j] that:[%s] words:[%s]"
-			,$that, $words);
-
-		print QF sprintf("- %9d *          > %s\n\n"
-			,$this, $words);
-
-	}
-}
-}
-	
-
-close AF;	
-close QF;	
 
 close TRC
    if($optctl->{'trace'});
@@ -292,216 +183,16 @@ sub Funcion
 	return $ret;
 }
 
-
-sub Test_GenNumSeq()
 {
-	my ($dummy)=@_;
-	my $ret='';
-	
-	&{$::debug}("Test_GenNumSeq( %s )", join(' ', map( (defined($_)?"[$_]":'<undef>'), @_)));
+my $sSep=' ';
 
-	# Cuerpo de Funcion
-	my @a;
-	@a = GenNumSeq(1,1);
-	&{$::debug}("a: [%s]", Dumper(\@a));
+sub Sep() { return $sSep; }
+sub SepSep($) { return $sSep=$_[0]; }
 
-	@a = GenNumSeq(1,4);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-
-	@a = GenNumSeq(2,4);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-
-	@a = GenNumSeq(3,4);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-
-	@a = GenNumSeq(3,6);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-	
-	@a = GenNumSeq(4,6);
-	&{$::debug}("a: [%s]", Dumper(\@a));
-	
-	&{$::debug}("Test_GenNumSeq()->[%s]", $ret);
-	return $ret;
-}
-
-sub GenNumSeq($$)
-{
-	my ($nNumLen, $nSeqLen)=@_;
-	my $ret='';
-	
-	&{$::debug}("GenNumSeq( %s )", join(' ', map( (defined($_)?"[$_]":'<undef>'), @_)));
-
-	# Cuerpo de Funcion
-	my $nLimit=10**$nNumLen;
-	my $sNumber = '' . int(rand($nLimit));
-	&{$::debug}("sNumber:[%s]", $sNumber);
-	
-	my $rhNum={ $sNumber => 1};
-
-	my ($i, $sOldDigit, $sNewDigit, $nPos);
-	while (scalar(keys(%{$rhNum})) < $nSeqLen) {
-		$nPos = int(rand($nNumLen));
-		$sOldDigit=substr($sNumber, $nPos, 1);
-		do {
-			$sNewDigit = '' . int(rand(10));
-		} while ($sNewDigit eq $sOldDigit);
-		substr($sNumber, $nPos, 1)=$sNewDigit;
-		&{$::debug}("sNumber:[%s]", $sNumber);
-		$rhNum->{$sNumber}=1;
-	}
-	
-	my @aSeq=keys(%{$rhNum});
-	
-	&{$::debug}("GenNumSeq()->[%s]", \@aSeq);
-	return @aSeq;
 }
 
 
-sub Test_Num2De()
-{
-	my ($dummy)=@_;
-	my $ret='';
-	
-	&{$::debug}("Test_Num2De( %s )", join(' ', map( (defined($_)?"[$_]":'<undef>'), @_)));
-
-	# Cuerpo de Funcion
-	my @aNums;
-	#@aNums= (0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20
-	#	,21,29,33,40,49,50,51,66,77,83,99);
-	#@aNums= (100,101,202,330,404,505,676,787,898,909);
-	#@aNums= (101,21);
-	# @aNums= (1000,2001,2023,3034,4045,5056,6767,7878,8989,9090);
-	@aNums= (0,1,2,10,11,19,20
-		,21,99,100,101,345,603,840,999
-		,1000,1001,2007,3010,9999,10000,10001,10010,10100,11000
-		,99999, 100000, 100001
-		,123456, 1234567, 2345678, 999999999
-		) ;
-
-	foreach my $n (@aNums) {
-		&{$::debug}("n:[%s] words:[%s]", $n, Num2De($n));
-		print sprintf("n:[%s] %s\n", $n, Num2De($n));
-	}
-	
-	&{$::debug}("Test_Num2De()->[%s]", $ret);
-	return $ret;
-}
-
-
-sub Num2De($)
-{
-	my ($num)=@_;
-	my $ret='';
-	
-	&{$::debug}("Num2De( %s )", join(' ', map( (defined($_)?"[$_]":'<undef>'), @_)));
-
-	# Cuerpo de Funcion
-	my (%cipher, %hTens);
-	%cipher = (	1 => "ein", 2 => "zwei", 3 => "drei"
-		,4 => "vier", 5 => "fünf", 6 => "sechs"
-		,7 => "sieben",8 => "acht", 9 => "neun"
-		,10 => "zehn", 11 => "elf", 12 => "zwölf"
-		,13 => "dreizehn", 14 => "veirzehn", 15 => "fünfzehn"
-		,16 => "sechzehn", 17 => "siebzehn", 18 => "achzehn"
-		,19 => "neunzehn" );
-	%hTens = (	1 => "zehn", 2 => "zwanzig",3 => "dreissig"
-		,4 => "vierzig", 5 => "fünfzig", 6 => "sechzig"
-		,7 => "siebzig", 8 => "achzig", 9 => "neunzig" );
-
-	my ($tens, $hundreds)=('','');
-
-	# (my $tens = $num) =~ s/^.*(\d\d)$/$1/;
-
-	if( $num == 0 ) {
-		$ret = "null";
-	} elsif( $num == 1 ) {
-		$ret = "eins";
-	} elsif( $num < 20 ) {
-		$ret = $cipher{$num} || "null";
-	} elsif( $num < 100 ) {
-		my $nRes=$num%10;
-		my $nTens=($num-$nRes)/10;
-		$ret = (($nRes == 1)? 'ein' : $cipher{$nRes}
-				.${sep}."und".${sep} )
-			if ($nRes>0);
-		$ret.= $hTens{$nTens};
-	} elsif( $num < 1000 ) {
-		my $nRes=$num%100;
-		my $nHund=($num-$nRes)/100;
-		$ret = $cipher{$nHund}.${sep}."hundert";
-		$ret.= ${sep}.Num2De($nRes)
-			if ($nRes>0);
-	} elsif( $num < 1000000 ) {
-		my $nRes=$num%1000;
-		my $nTaus=($num-$nRes)/1000;
-		$ret = Num2De($nTaus).${sep} ."tausend";
-		$ret.= ${sep}.Num2De($nRes)
-			if ($nRes>0);
-	} elsif( $num < 1000000000 ) {
-		my $nRes=$num%1000000;
-		my $nMill=($num-$nRes)/1000000;
-		$ret='ein Million';
-		$ret=Num2De($nMill).${sep}.'Millionen'
-			if ($nMill>1);
-		$ret.= ${sep}.Num2De($nRes)
-			if ($nRes>0);
-	} else {
-		$ret=$num;
-
-	}
-	
-	&{$::debug}("Num2De()->[%s]", $ret);
-	return $ret;
-}
-
-# /usr/local/share/perl5/Date/Spoken/German.pm
-# sub yeartospoken
-sub yeartospoken
-{
-	my $year = shift;
-	(my $tens = $year) =~ s/^.*(\d\d)$/$1/;
-
-	my %cipher = (	1 => "ein", 2 => "zwei", 3 => "drei"
-		,4 => "vier", 5 => "fünf", 6 => "sechs"
-		,7 => "sieben",8 => "acht", 9 => "neun"
-		,10 => "zehn", 11 => "elf", 12 => "zwölf"
-		,13 => "dreizehn", 14 => "veirzehn", 15 => "fünfzehn"
-		,16 => "sechzehn", 17 => "siebzehn", 18 => "achzehn"
-		,19 => "neunzehn" );
-	my %hTens = (	1 => "zehn", 2 => "zwanzig",3 => "dreissig"
-		,4 => "vierzig", 5 => "fünfzig", 6 => "sechzig"
-		,7 => "siebzig", 8 => "achzig", 9 => "neunzig" );
-
-
-	my $hundreds = "";
-	if( $year < 10 ) {
-		$year = $cipher{$year} || "null";
-	} else {
-		if( $tens == 0 ) {
-			$tens = "";
-		} elsif( ($tens % 10) == 0 ) {
-			$tens =~ s/(.)(.)/$hTens{$1}/;
-		} else {
-			if( $tens < 10 ) {
-				$tens =~ s/(.)(.)/$cipher{$2}/;
-			} else {
-				$tens =~ s/(.)(.)/$cipher{"$1$2"} || $cipher{$2}.(($1==1)?$hTens{$1}:"und".$hTens{$1})/e;
-			}
-		}
-		if( $year >= 100 ) {
-			($hundreds = $year) =~ s/^(.?.)..$/$1/;
-			if( $hundreds >= 20 || $hundreds == 10 ) {
-				$hundreds =~ s/(.)(.)/$cipher{$1}."tausend".(($2>0)?$cipher{$2}."hundert":"")/ex;
-			} else {
-				if( $hundreds > 10 ) {
-					$hundreds =~ s/(.)(.)/($cipher{"$1$2"} || $cipher{$2}."zehn")."hundert"/e;
-				} else {
-					$hundreds = $cipher{$hundreds}."hundert";
-				}
-			}
-		}
-	}
-	return $hundreds.$tens;
-}
-
+#####
+# EOF
+#####
 
